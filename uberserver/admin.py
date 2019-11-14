feb5acff495f92783aa48f179e75051e396e757f
decole <decole.satiadarshi@gmail.com>
2019-11-15 00:07:37 +0400

    add models swifts and sensors, config postgreSQL

---
+++ uberserver/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Link)
+admin.site.register(models.Type)
+admin.site.register(models.Format)
+admin.site.register(models.Sensor)
+admin.site.register(models.Swift)
