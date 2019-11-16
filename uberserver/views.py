1537b484c1ff3fce1aa8e7dc6ccd6dccd4a77e64
decole <decole.satiadarshi@gmail.com>
2019-11-17 02:27:33 +0400

    add simple pages to watch sensors and swifts

--- uberserver/views.py
+++ uberserver/views.py
@@ -3,6 +3,36 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from uberserver.models import Sensor, Swift
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. <a href='/admin/'>/admin/</a>")
+    return HttpResponse("""Hello, world. You're at the polls index <br> 
+    <a href='/admin/'>/admin/</a> <br>
+    <a href='/sensors/'>/sensors/</a> <br>
+    <a href='/swifts/'>/swifts/</a> <br>
+    """)
+
+
+def sensors(request):
+    sensor_list = Sensor.objects.all()
+    model = {'title': 'Sensors', 'type_object': 'sensor', 'sensor_list': sensor_list}
+    return render(request, 'pages/sensors/sensors.html', model)
+
+
+def swifts(request):
+    swift_list = Swift.objects.all()
+    model = {'title': 'Swifts', 'type_object': 'swift', 'swift_list': swift_list}
+    return render(request, 'pages/swifts/swifts.html', model)
+
+
+def sensor(request, id_sensor):
+    sensor_one = Sensor.objects.get(id=id_sensor).get_sensor_data()
+    model = {'title': 'Sensor', 'type_object': 'sensor', 'sensor': sensor_one}
+    return render(request, 'pages/sensors/sensor.html', model)
+
+
+def swift(request, id_swift):
+    swift_one = Swift.objects.get(id=id_swift).get_swift_data()
+    model = {'title': 'Swift', 'type_object': 'swift', 'swift': swift_one}
+    return render(request, 'pages/swifts/swift.html', model)
